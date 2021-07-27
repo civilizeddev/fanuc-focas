0bd098966d348840ae38be691953d85351d61879
David Lee <david.lee@unomic.com>
2021-07-27 14:18:20 +0900

    test(fwlib32): add test cases (not working)

---
+++ src/test/scala/fwlib32/Fwlib32TestSuite.scala
@@ -0,0 +1,17 @@
+package fwlib32
+
+import com.sun.jna.NativeLong
+
+import java.nio.ShortBuffer
+
+class Fwlib32TestSuite extends munit.FunSuite {
+  test("connect") {
+    val fwlib32 = Fwlib32.INSTANCE
+    val handle  = ShortBuffer.allocate(1)
+    val result1 = fwlib32.cnc_allclibhndl3("127.0.0.1", 8193, new NativeLong(10L), handle)
+    assertEquals(result1, Fwlib32.EW_OK)
+    println(handle.get(0))
+    val result2 = fwlib32.cnc_freelibhndl(handle.get(0))
+    assertEquals(result2, Fwlib32.EW_OK)
+  }
+}
